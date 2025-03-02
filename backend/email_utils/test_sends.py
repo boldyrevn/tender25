27381c908a024712bed0c918ad5178c827ce687d
ebulatov <yabulatov@mts.ru>
2025-03-02 09:17:35 +0300

    add openpyxl module / convert CSV/XLSX

--- backend/email_utils/test_sends.py
+++ backend/email_utils/test_sends.py
@@ -1,5 +1,6 @@
 from email_utils.standart_email_message import send_standard_email
 import pandas as pd
+import os
 def main():
 
     # Создаем тестовые данные
@@ -13,15 +14,24 @@ def main():
     # Создаем DataFrame
     df = pd.DataFrame(data)
 
+    report_4email = f'metric_report.xlsx' # Название файла, c ФОРМАТОМ ФАЙЛА!!!
+    #df.to_csv(report_4email, index=False)
+    #df.to_csv(report_4email, index=False)
+
+    date = '02.03.2025'
+
     sender = 'm2206664@edu.misis.ru'
-    subject = 'Тестовая отправка нескольким людям'
-    html_content = 'Привет! Проверим подятянулся ли русский язык<br>'
-    html_content += f"<h3>Работоспособность конструктора и проверка шрифтов<br>"
-    html_content += 'Таблица с какими-то рандомными данными<br>'
+    subject = f'Регулярный отчет по метрике: "Оборот за период" от {date}'
+    html_content = 'Добрый день! Отчет по метрике "Оборот за период" выгружен<br>'
+    html_content += f"Файл с отчетностью по метрике в виде таблицы приложен в теле письма<br>"
+    html_content += 'Краткий срез представлен в таблице ниже:<br>'
     html_content += " "
     html_content += df.to_html(index=False, justify="left").replace("\n", "")
-    recipients = [sender, 'bulatove106@gmail.com']
-    send_standard_email(recipients=recipients, sender=sender, subject=subject, content=html_content)
+    recipients = [sender, 'bulatove106@gmail.com', 'm2110542@edu.misis.ru']
+    files = [report_4email]
+    send_standard_email(recipients=recipients, sender=sender, subject=subject, content=html_content, files=files)
+
+    os.remove(report_4email)
 
 
 if __name__ == "__main__":
